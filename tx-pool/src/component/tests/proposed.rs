9482c17e2329c2914973de52c3410555af4591c3
yukang <moorekang@gmail.com>
2023-05-06 15:39:12 +0800

    add bench for container

--- tx-pool/src/component/tests/proposed.rs
+++ tx-pool/src/component/tests/proposed.rs
@@ -1,3 +1,5 @@
+extern crate test;
+
 use crate::component::tests::util::{
     build_tx, build_tx_with_dep, build_tx_with_header_dep, DEFAULT_MAX_ANCESTORS_COUNT,
     MOCK_CYCLES, MOCK_FEE, MOCK_SIZE,
@@ -690,4 +692,116 @@ fn test_max_ancestors_with_dep() {
 
     assert_eq!(pool.edges.inputs_len(), 1);
     assert_eq!(pool.edges.outputs_len(), 1);
+}
+
+use std::hint::black_box;
+use test::Bencher;
+#[bench]
+fn test_container_bench_add_remove(bench: &mut Bencher) {
+    use crate::component::container::SortedTxMap;
+    let mut map = SortedTxMap::new(DEFAULT_MAX_ANCESTORS_COUNT);
+    let tx1 = TxEntry::dummy_resolve(
+        TransactionBuilder::default().build(),
+        100,
+        Capacity::shannons(100),
+        100,
+    );
+    let tx2 = TxEntry::dummy_resolve(
+        TransactionBuilder::default()
+            .input(
+                CellInput::new_builder()
+                    .previous_output(
+                        OutPoint::new_builder()
+                            .tx_hash(tx1.transaction().hash())
+                            .index(0u32.pack())
+                            .build(),
+                    )
+                    .build(),
+            )
+            .witness(Bytes::new().pack())
+            .build(),
+        200,
+        Capacity::shannons(200),
+        200,
+    );
+    let tx3 = TxEntry::dummy_resolve(
+        TransactionBuilder::default()
+            .input(
+                CellInput::new_builder()
+                    .previous_output(
+                        OutPoint::new_builder()
+                            .tx_hash(tx2.transaction().hash())
+                            .index(0u32.pack())
+                            .build(),
+                    )
+                    .build(),
+            )
+            .witness(Bytes::new().pack())
+            .build(),
+        200,
+        Capacity::shannons(200),
+        200,
+    );
+    bench.iter(|| {
+        //let t = std::time::Instant::now();
+        for _ in 0..1000 {
+            let tx1_id = tx1.proposal_short_id();
+            let tx2_id = tx2.proposal_short_id();
+            let tx3_id = tx3.proposal_short_id();
+            map.add_entry(black_box(tx1.clone())).unwrap();
+            map.add_entry(black_box(tx2.clone())).unwrap();
+            map.add_entry(black_box(tx3.clone())).unwrap();
+            map.remove_entry(&tx1_id);
+            map.remove_entry(&tx2_id);
+            map.remove_entry(&tx3_id);
+        }
+        //eprintln!("{:0.2?}", t.elapsed())
+    });
+}
+
+#[bench]
+fn test_container_bench_sort(bench: &mut Bencher) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    use crate::component::container::SortedTxMap;
+    let mut map = SortedTxMap::new(1000000);
+    let tx1 = TxEntry::dummy_resolve(
+        TransactionBuilder::default().build(),
+        100,
+        Capacity::shannons(100),
+        100,
+    );
+    map.add_entry(black_box(tx1.clone())).unwrap();
+    let mut prev_tx = tx1.clone();
+    for _ in 0..1000 {
+        let next_tx = TxEntry::dummy_resolve(
+            TransactionBuilder::default()
+                .input(
+                    CellInput::new_builder()
+                        .previous_output(
+                            OutPoint::new_builder()
+                                .tx_hash(prev_tx.transaction().hash())
+                                .index(0u32.pack())
+                                .build(),
+                        )
+                        .build(),
+                )
+                .witness(Bytes::new().pack())
+                .build(),
+            rng.gen_range(0..1000),
+            Capacity::shannons(200),
+            rng.gen_range(0..1000),
+        );
+        map.add_entry(black_box(next_tx.clone())).unwrap();
+        prev_tx = next_tx;
+    }
+
+    bench.iter(|| {
+        //let t = std::time::Instant::now();
+        for _ in 0..100 {
+            let _entries = black_box(map.score_sorted_iter().collect::<Vec<_>>());
+        }
+        //eprintln!("{:0.2?}", t.elapsed())
+    });
 }
