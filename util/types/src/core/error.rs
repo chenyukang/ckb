5deace6b4ad97f4726f7b59b83f5c60cc390cf81
Yukang <moorekang@gmail.com>
2026-07-12 10:47:54 +0800

    chore: improve DaoScriptSizeVerifier consistency

--- util/types/src/core/error.rs
+++ util/types/src/core/error.rs
@@ -210,6 +210,13 @@ pub enum TransactionError {
         index: usize,
     },
 
+    /// Nervos DAO output data mismatch.
+    #[error("The DAO output data at index {} is inconsistent", index)]
+    DaoOutputDataMismatch {
+        /// The index of inconsistent DAO output data.
+        index: usize,
+    },
+
     /// The lock script hash type is not permitted by the current consensus rules.
     #[error(
         "The lock script hash type {} is not permitted by the current consensus rules.",
@@ -259,6 +266,7 @@ impl TransactionError {
             | TransactionError::MismatchedVersion { .. }
             | TransactionError::Compatible { .. }
             | TransactionError::DaoLockSizeMismatch { .. }
+            | TransactionError::DaoOutputDataMismatch { .. }
             | TransactionError::Internal { .. } => false,
         }
     }
